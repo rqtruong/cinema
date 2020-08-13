a9532165302533ee81357316f9ae1ed1de5b5a7a
Richard Truong <rqtruong@uci.edu>
2020-08-12 20:41:15 -0700

    Use API, end of lesson 30

--- src/main.js
+++ src/main.js
@@ -5,11 +5,15 @@ import genres from './util/genres';
 import MovieList from './components/MovieList.vue';
 import MovieFilter from './components/MovieFilter.vue';
 
+import VueResource from 'vue-resource';
+Vue.use(VueResource);
+
 new Vue({
     el: "#app",
     data: {
         genre: [],
         time: [],
+        movies: []
     },
     methods: {
         checkFilter(category, title, checked) {
@@ -26,5 +30,10 @@ new Vue({
     components: {
         MovieList,
         MovieFilter
+    },
+    created() {
+        this.$http.get('/api').then(response => {
+            this.movies = response.data;
+        });
     }
 });
