6296008c45cafbb2eeb5c9dc1ef198a4bb53ab6d
Richard Truong <rqtruong@uci.edu>
2020-08-12 21:46:09 -0700

    display session times and filter by time, end of lesson 37

--- src/components/MovieItem.vue
+++ src/components/MovieItem.vue
@@ -8,12 +8,39 @@
                 <h2>{{movie.Title}}</h2>
                 <span class="movie-rating">{{movie.Rated}}</span>
             </div>
+            <div class="movie-sessions">
+                <div v-for="session in filteredSessions(sessions)" class="session-time-wrapper">
+                    <div class="session-time">
+                        {{formatSessionTime(session.time)}}
+                </div>
+            </div>
         </div>
     </div>
 </template>
 
 <script>
+import times from '../util/times';
 export default {
-    props: ['movie']
+    props: ['movie', 'sessions', 'day', 'time'],
+    methods: {
+        formatSessionTime(raw) {
+            return this.$moment(raw).format('h:mm A')
+        },
+        filteredSessions(sessions) {
+            return sessions.filter(this.sessionPassesTimeFilter);
+        },
+        sessionPassesTimeFilter(session) {
+            if (!this.day.isSame(this.$moment(session.time), 'day')) {
+                return false;
+            }
+            if (this.time.length === 0 || this.time.length === 2) {
+                return true;
+            }
+            if (this.time[0] === times.AFTER_6PM) {
+                return this.$moment(session.time).hour() >= 18;
+            }
+            return this.$moment(session.time).hour() < 18;
+        }
+    }
 }
 </script>
