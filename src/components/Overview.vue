bdc48181ed1268cc5f5b6d26f7f2547ea19b2fb7
Richard Truong <rqtruong@uci.edu>
2020-08-13 19:36:41 -0700

    add day select and filter, end of lesson 54

--- src/components/Overview.vue
+++ src/components/Overview.vue
@@ -1,5 +1,6 @@
 <template>
     <div id="overview">
+        <day-select v-bind:selected="day"></day-select>
         <div id="main">
             <movie-list v-bind:genre="genre" v-bind:time="time" v-bind:movies = "movies" v-bind:day="day"></movie-list>
             <movie-filter></movie-filter>
@@ -10,12 +11,14 @@
 <script>
     import MovieList from './MovieList.vue';
     import MovieFilter from './MovieFilter.vue';
+    import DaySelect from './DaySelect.vue';
 
     export default {
         props: ['genre', 'time', 'movies', 'day'],
         components: {
             MovieList,
-            MovieFilter
+            MovieFilter,
+            DaySelect
         },
     }
 </script>
